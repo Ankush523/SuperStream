ccecac96068da4466c6a2d662e7a8299378bb21e
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-09-04 00:09:09 +0530

    timestamp change

--- src/Streams/RealTimeBalance/PresentBalance.js
+++ src/Streams/RealTimeBalance/PresentBalance.js
@@ -3,10 +3,13 @@ import GetProvider from '../../hooks/GetProvider'
 import { Framework } from "@superfluid-finance/sdk-core";
 
 const PresentBalance = () => {
-
+    const [balance,setBalance] = useState(0)
     const provider = GetProvider();
 
     const getSuperfluid=async()=>{
+        var time = Date.now();
+        time = (time.toString()).slice(0,-3);
+        console.log(time)
         const sf = await Framework.create({
             chainId: 80001, // you can also use chainId here instead
             provider: provider
@@ -14,26 +17,24 @@ const PresentBalance = () => {
 
           const fDAIx = await sf.loadSuperToken("0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f");
 
-          var bal = await fDAIx.balanceOf({
-            account: "0x0fdcF87dBEE21d938944aFC3aD0cbCc271E8a5b4",
-            providerOrSigner: provider
-          });
 
           var rbal = await fDAIx.realtimeBalanceOf({
             account: "0x0fdcF87dBEE21d938944aFC3aD0cbCc271E8a5b4",
-            timestamp: "1662227030",
+            timestamp: time,
             providerOrSigner: provider
           });
 
 
-        console.log(bal)
+        console.log(rbal)
+        
+        setBalance(rbal.availableBalance)
     }
 
-    const [balance,setBalance] = useState(0)
+    
 
   return (
     <div>
-        <button >Present Balance</button>
+        <button onClick={()=>getSuperfluid()}>Present Balance : {(balance.toString())}</button>
 
         <a href='/deleteflow'><button>Delete Flow</button></a>
     </div>
