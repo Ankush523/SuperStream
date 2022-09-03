634e2b5fbb2a9a230b062b832a442be6073366d5
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-09-03 19:55:22 +0530

    import changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,14 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
 import React from "react";
 const Home = () => {
   return (
-    <div >
+    <div>
       {/* <img src={Bg} className="w-[100vw] h-[100vh]" /> */}
-      <div >
-        <label >
-          Stream your Recurring Payments
-        </label>
-        <div >
-          <a href="/startpage"><button>Let's Get Started</button></a>
-          <ConnectButton/>
+      <div>
+        <label>Stream your Recurring Payments</label>
+        <div>
+          <a href="/startpage">
+            <button>Let's Get Started</button>
+          </a>
         </div>
       </div>
     </div>
